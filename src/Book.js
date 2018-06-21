beeec3bb17d80e6fc339c410ade130cdf8652875
Alex Wine <alexwine36@gmail.com>
2018-06-20 22:19:23 -0600

    feat: Changed to components to render Books based on props instead of static content

---
+++ src/Book.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Book extends Component {
+  render() {
+    const { title, authors, imageLinks } = this.props.book;
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 188,
+                backgroundImage: `url(${imageLinks.smallThumbnail})`
+              }}
+            />
+            <div className="book-shelf-changer">
+              <select>
+                <option value="move" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{title}</div>
+          <div className="book-authors">{authors[0]}</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default Book;
