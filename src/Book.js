cd78252fa1121460329719f6bb35224e329341e6
Alex Wine <alexwine36@gmail.com>
2018-06-21 08:29:18 -0600

    feat: Added an update method for selecting the shelf value on individual books

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
+import { update } from "./BooksAPI";
 
 class Book extends Component {
+  state = {
+    shelf: this.props.book.shelf || "none"
+  };
+
+  handleReadChange(e) {
+    const { book } = this.props;
+    const target = e.target;
+    const value = target.value;
+    console.log(value, book);
+    this.setState(() => ({
+      shelf: value
+    }));
+
+    update(book, value).then(books => {
+      console.log(books);
+    });
+  }
+
   render() {
     const { title, authors, imageLinks } = this.props.book;
+    const { shelf } = this.state;
     return (
       <li>
         <div className="book">
@@ -16,7 +36,12 @@ class Book extends Component {
               }}
             />
             <div className="book-shelf-changer">
-              <select>
+              <select
+                value={shelf}
+                onChange={e => {
+                  this.handleReadChange(e);
+                }}
+              >
                 <option value="move" disabled>
                   Move to...
                 </option>
